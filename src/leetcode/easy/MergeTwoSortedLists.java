1ed9b80ef83ab908a5547bb0a3fa6550c842d9ef
jasonborn <2286842225@qq.com>
2020-08-08 18:00:56 +0800

    算法

--- src/leetcode/easy/MergeTwoSortedLists.java
+++ src/leetcode/easy/MergeTwoSortedLists.java
@@ -1,5 +1,8 @@
 package leetcode.easy;
 
+/**
+ * 合并有序链表
+ */
 public class MergeTwoSortedLists {
     public class ListNode {
       int val;
@@ -12,4 +15,19 @@ public class MergeTwoSortedLists {
       }
     }
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        if(l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
